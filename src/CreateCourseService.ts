7313355b3ee4a91f19217b45ec58a64953e58b9a
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2022-05-29 21:29:56 -0300

    configurando padroes de projeto

--- src/CreateCourseService.ts
+++ src/CreateCourseService.ts
@@ -13,3 +13,4 @@ class CreateCourseService {
 }
 
 export default new CreateCourseService()
+// padrão singleton: exportar com new posso importar quantas vezes eu quiser essa classe que só haverá uma instância
